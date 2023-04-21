c122a3c4630e4834e12c0afcc0d782b14c7942cb
Franklyn inoma <125371243+Franklyn101@users.noreply.github.com>
2023-04-21 11:10:01 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,270 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="parent">
+    <div class="nav-logo">
+      <img src="https://assets2.razerzone.com/images/phoenix/razer-ths-logo.svg" alt="">
+
+      
+        <nav>
+          <a href="">Store</a>
+          <a href="">PC</a>
+          <a href="">Console</a>
+          <a href="">Moblie</a>
+          <a href="">Lifestyles</a>
+          <a href="">services</a>
+          <a href="">Community</a>
+          <a href="">Support</a>
+        </nav>
+        <div class="icons">
+          <span class="input">
+          <span class="material-symbols-outlined" style="color: #888;">
+            search
+</span>
+          <!-- <input type="search" placeholder="search"> -->
+</span>
+
+         <a href=""><img src="https://www.razer.com/assets/images/icons/cart_icon.svg" alt=""></a>
+
+        </div>
+      </div>
+  </div>
+        <div class="grey">
+          <p>Unleash a new age of technology with an arsenal of cutting-edge innovations that will keep you ahead of the game. Shop Now ></p>
+
+        </div>
+       
+
+      
+   
+
+<section class="image-section">
+   <!-- image1 -->
+   <div class="image1">
+          <img src="https://assets2.razerzone.com/images/pnx.assets/2d7c19fedce91a875fc6a861e251ad30/razer-blade-16-homepage-desktop.jpg">
+  <span class="info">
+    <h1>NEW RAZER BLADE 16</h1>
+    <h2>BIGGER. BETTER. STILL THE BEST</h2>
+    <span class="link">
+      <a  href="#">Learn more <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      <a  href="#">Buy <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt=""></a>
+    </span>  
+  </span>
+    </div>
+  
+
+ 
+     <!-- image2 -->
+   <div class="image2">
+          <img src="https://assets2.razerzone.com/images/pnx.assets/4ef59df8938b4c9a37a3ca3aa577b1e9/stream-controller-x-homepage-desktop.jpg">
+  <span class="info2">
+    <h1>RAZER STREAM CONTROLLER X</h1>
+    <h2>INSTANT CONTROL.INFINITE CREATIVITY.</h2>
+    <span class="link2">
+      <a  href="#">Learn more <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      <a  href="#">Buy <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt=""></a>
+    </span>  
+  </span>
+    </div>
+
+
+       <!-- image3 -->
+   <div class="image3">
+
+          <img src="https://assets2.razerzone.com/images/pnx.assets/2d7c19fedce91a875fc6a861e251ad30/razer-blade-18-homepage-desktop.jpg">
+  <span class="info3">
+    <h1>NEW RAZER BLADE 18</h1>
+    <h2>THE ULTIMATE DESKTOP REPLACEMENT</h2>
+    <span class="link3">
+      <a  href="#">Learn more <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      <a  href="#">Buy <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt=""></a>
+    </span>  
+  </span>
+    </div>
+
+
+    <!-- image4and5 -->
+<div class="image4-5">
+  <div class="image4">
+    <img src="https://assets2.razerzone.com/images/pnx.assets/1b1e2970306c932a02bcb6d4824f0364/new-age-technology-campaign-b-950x580-desktop.jpg" alt="">
+    <span class="link4">
+    <h1>UNLEASH A NEW AGE OF TECHNOLOGY</h1>
+     <h2>EXPERIENCE THE REVOLUTION</h2>
+    <a  href="#">Shop Now <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      
+    </span>
+
+  </div>
+
+    <div class="image5">
+      <img src="https://assets2.razerzone.com/images/pnx.assets/84d0946021d11b959bda0545c4d4c234/enkipro-lamborghini-b-950x580-desktop.jpg" alt="">
+      <span class="link5">
+    <h1>RAZER ENKI PRO</h1>
+     <h2>AUTOMOBLI LAMBOURGHINI EDITION</h2>
+     <span class="linkk">
+    <a  href="#">Learn more <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      <a  href="#">Buy <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt=""></a>
+    </span>
+    </span>
+
+    </div>
+  
+  
+</div>
+
+<!-- image6and7 -->
+
+
+<div class="image6-7">
+  <div class="image6">
+    <img src="https://assets2.razerzone.com/images/pnx.assets/3474002077a69ea3690d1a798c669b13/razer-blade-15-2023-b-950x580-desktop.jpg" alt="">
+    <span class="link6">
+    <h1>NEW RAZER BLADE 15</h1>
+     <h2>PORTABLE.POWERFUL.PERFECTION</h2>
+     <span class="linkk">
+    <a  href="#">Learn more <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      <a  href="#">Buy <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt=""></a>
+    </span>
+    </span>
+  </div>
+
+    <div class="image7">
+      <img src="https://assets2.razerzone.com/images/pnx.assets/1b1e2970306c932a02bcb6d4824f0364/razer-basilisk-v3-x-hyperspeed-b-950x580-desktop.jpg" alt="">
+      <span class="link7">
+    <h1>RAZER BASLISK V3 X HYPERSPEED</h1>
+     <h2>ADVANCED CUSTOMIZABILITY.UNLEASHED</h2>
+     <span class="linkk">
+    <a  href="#">Learn more <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      <a  href="#">Buy <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt=""></a>
+    </span>
+    </span>
+    </div>
+  
+</div>
+
+<!-- image8and9 -->
+<div class="image8-9">
+  <div class="image8">
+   
+    <img src="https://assets2.razerzone.com/images/pnx.assets/dec1968e4414aa9dd00718ae5c3f531b/razer-edge-b-950x580-desktop.jpg" alt="">
+    <span class="link8">
+      <button>RAZER.COM <b>EXCLUSIVE</b></button>
+      
+    <h1>RAZER EDGE</h1>
+    
+     <h2>THE ULTIMATE ANDROID GAMING HANDHELD</h2>
+     
+     <span class="linkk">
+    <a  href="#">Learn more <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      <a  href="#">Buy <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt=""></a>
+      
+    </span>
+    </span>
+  </div>
+
+    <div class="image9">
+      <img src="https://assets2.razerzone.com/images/pnx.assets/1b1e2970306c932a02bcb6d4824f0364/razer-meta-quest-2-vr-b-950x580-desktop.jpg" alt="">
+      <span class="link9">
+    <img src="https://assets2.razerzone.com/images/pnx.assets/dec1968e4414aa9dd00718ae5c3f531b/razer-edge-b-950x580-desktop.jpg" alt="">
+    <h1>RAZER VR ACCESSORIES</h1>
+     <h2>AUTHORIZED FOR META QUEST 2</h2>
+     <span class="linkk">
+    <a  href="#">Learn more <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt="" class=""></a>
+      <a  href="#">Buy <img src="https://www.razer.com/assets/images/icons/icon-arrow-right.svg" alt=""></a>
+    </span>
+    </span>
+    </div>
+  
+</div>
+<footer>
+  <div class="foot">
+  <div class="list">
+    
+  <h4>Shop</h4>
+<a href="">RazerStores</a>
+<a href="">RazerCafe</a>
+<a href="">Store Locator</a>
+<a href="">Purchase Programs</a>
+<a href="">Education</a>
+<a href="">Exclusives</a>
+<a href="">RazerStore Rewards</a>
+<a href="">Newsletter</a>
+    
+  </div>
+
+  <div class="list2">
+  
+    <h4>Explore</h4>
+<a href="">Technology</a>                      
+<a href="">Chroma RGB</a>
+<a href="">Concepts</a>
+<a href="">Esports</a>
+<a href="">Collabs</a> 
+  
+  </div>
+
+<div class="list">
+    
+  <h4>Support</h4>
+<a href="">Get Help</a>
+<a href="">Registration & Warrant</a>
+<a href="">RazerStore Support</a>
+<a href="">Purchase Programs</a>
+<a href="">RazerCare</a>
+<a href="">Manage Razer ID</a>
+<a href="">Support Videos</a>
+<a href="">Accessibility Statement</a>
+    
+  </div>
+
+<div class="list2">
+    
+    <h4>Company</h4>
+<a href="">About Us</a>                      
+<a href="">Careers</a>
+<a href="">Press Room</a>
+<a href="">Zventures</a>
+<a href="">Contact Us</a> 
+  
+  </div>
+  <div class="media">
+    <h6>FOR GAMEs. BY GAMERS.&trade;</h6>
+    <span>
+    <img src="https://assets2.razerzone.com/images/phoenix/SM0001-facebook.svg" alt="">
+    <img src="https://assets2.razerzone.com/images/phoenix/SM0003-instagram.svg" alt="">
+    <img src="https://assets2.razerzone.com/images/phoenix/SM0005-twitter.svg" alt="">
+  </span>
+  </div>
+  
+
+
+  </div>
+  <div class="bottom">
+    <div class="right">
+      <h6>Copyright © 2023 Razer Inc. All rights reserved.
+</h6>
+    <a href="#">Site Map |</a>
+    <a href="#">Legal Terms |</a>
+    <a href="#">Privacy Policy |</a>
+    <a href="#">Cookie Setting |</a>
+    
+
+</div>
+    <div class="state">
+      <p>United States |</p>
+      <a href="#">Change Location></a>
+
+    </div>
+  </div>
+  
+</footer>
+
+  </section>
+    
+
+</template>
+
+
+
